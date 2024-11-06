e569e45c1eb1022909370c81f7c0e5b970f8a6e6
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2024-11-06 20:39:56 -0300

    feat: add all factories

--- app/database/migrations/2024_11_06_042549_create_vaccines_table.php
+++ app/database/migrations/2024_11_06_042549_create_vaccines_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('vaccines', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
-            $table->string('batch_number', 20);
+            $table->string('batch', 45);
             $table->date('expiration_date');
             $table->timestamps();
             $table->softDeletes();
